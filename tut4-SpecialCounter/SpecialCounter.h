8574d442fdf28a136037651395612dd1f6517b8f
Devashnee <devashneebhagawandin@hotmail.com>
2016-03-13 00:11:06 +0200

    functions in header

--- tut4-SpecialCounter/SpecialCounter.h
+++ tut4-SpecialCounter/SpecialCounter.h
@@ -2,13 +2,25 @@
 class SpecialCounter
 {
 	private:
-		int Start;		//stores the starting value
-		int end;		//stores the stopping value
+		int Start;		//stores the first value
+		int end;		//stores the ending/stopping value
 		int increment;		//stores the increment value
 		int decrement;		//stores the decrement value
 		int currValue;	//stores the current value of the counter
+
 public:
-	SpecialCounter();
+public:
+
+	SpecialCounter();		//default constructor
+	SpecialCounter(int, int, int, int);
+
+	void start();			//changes the Starting 
+	void stop();			//changes the Stopping 
+	void stepInc(int);		//changes the Increment
+	void stepDec(int);		//changes the Decrement 
+	void operator++();		//counts Up by one
+	void operator--();		//counts Down by one
+
 	~SpecialCounter();
 };
 
